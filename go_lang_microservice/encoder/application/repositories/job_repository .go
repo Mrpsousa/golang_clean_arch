ee4dc97ed17b0ab85a1cf95379b4746441ba0244
Mrpsousa <urameshi.uba@gmail.com>
2022-06-05 09:14:51 -0300

    feature: job repo and tests

---
+++ go_lang_microservice/encoder/application/repositories/job_repository .go
@@ -0,0 +1,54 @@
+package repositories
+
+import (
+	"encoder/domain"
+	"github.com/jinzhu/gorm"
+	"fmt"
+)
+
+type JobRepository interface {
+	Insert(job *domain.Job) (*domain.Job, error)
+	Find(id string) (*domain.Job, error)
+	Update(job *domain.Job) (*domain.Job, error)
+}
+ 
+type JobRepositoryDb struct {
+	Db *gorm.DB
+}
+
+// func NewVideoRepository(db *gorm.DB) *VideoRepositoryDb {
+// 	return &VideoRepositoryDb{Db: db}
+// }
+
+func (repo JobRepositoryDb) Insert(job *domain.Job) (*domain.Job, error) {
+
+	err := repo.Db.Create(job).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return job, nil
+}
+
+func (repo JobRepositoryDb) Find(id string) (*domain.Job, error){
+
+	var job domain.Job
+	repo.Db.Preload("Video").First(&job, "id = ?", id)
+
+	if job.ID == ""{
+		return nil, fmt.Errorf("job does not exist")
+	}
+
+	return &job, nil
+}
+
+func (repo JobRepositoryDb) Update(job *domain.Job) (*domain.Job, error){
+	err := repo.Db.Save(&job).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return job, nil
+}
